1dc56f7f2d4c15243d38b65272cfc9d289f06034
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-04-27 02:16:25 +0000

    Code along 3

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -11,6 +11,40 @@ Contact.destroy_all
 # - Insert and read contact data for companies in the database
 
 # 1. insert new rows in the contacts table with relationship to a company
+puts "Companies: #{Company.all.count}"
+puts "Contacts: #{Contact.all.count}"
+
+apple = Company.find_by({ "name" => "Apple" })
+puts apple.inspect
+
+cook = Contact.new
+cook["first_name"] = "Tim"
+cook["last_name"] = "Cook"
+cook["email"] = "tim@apple.com"
+cook["company_id"] = apple["id"]
+cook.save
+
+#puts cook.inspect
+
+craig = Contact.new
+craig["first_name"] = "Craig"
+craig["last_name"] = "Federighi"
+craig["email"] = "craig@apple.com"
+craig["company_id"] = apple["id"]
+craig.save
+
+amazon = Company.find_by({ "name" => "Amazon" })
+
+bezos = Contact.new
+bezos["first_name"] = "Jeff"
+bezos["last_name"] = "Bezos"
+bezos["email"] = "jeff@blue.com"
+bezos["company_id"] = amazon["id"]
+bezos.save
+
+puts "There are #{Company.all.count} companies"
+puts "There are #{Contact.all.count} contacts"
+
 
 # 2. How many contacts work at Apple?
 
